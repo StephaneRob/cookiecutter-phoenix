5c7585dc28edc184f4c6d63e907792024a5f3e51
Stéphane ROBINO <stephane.robino@gmail.com>
2018-03-12 13:14:03 +0100

    Optional DB setup

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -9,8 +9,13 @@ git commit -m 'Initial commit'
 echo -e "\n--------- INSTALL HEX PACKAGES ---------"
 mix deps.get
 
+{% if cookiecutter.database_setup == 'setup' %}
 echo -e "\n--------- CREATE DB AND MIGRATE ---------"
 mix ecto.setup
+{% elif cookiecutter.database_setup == 'migrate' %}
+echo -e "\n--------- MIGRATE DB ---------"
+mix ecto.migrate
+{% endif %}
 
 echo -e "\n--------- INSTALL ASSETS ---------"
-cd assets && npm install
+cd assets && yarn install
